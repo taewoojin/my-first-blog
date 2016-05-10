39400b94c24fa7b45e88ca59a309c21d15efb8e8
Taewoo <qkqnrpa@gmail.com>
2016-05-10 17:54:19 +0900

    Added comment model

--- blog/views.py
+++ blog/views.py
@@ -35,8 +35,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
+            post.publish()
             return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
